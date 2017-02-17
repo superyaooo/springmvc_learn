25b42d434cc4d3cd9cb0f425a83373c23325bd30
superyaooo <superyaooo@gmail.com>
2017-02-16 23:06:14 -0600

    chaining and static views for static files

--- FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
@@ -14,8 +14,14 @@ public class MinutesController {
 		
 		System.out.println("exercise: " + exercise.getMinutes());
 		
-		return "addMinutes";
+		return "addMinutes.html";
 	}
 	
+//	@RequestMapping(value = "/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//		
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		
+//		return "addMinutes";
+//	}
 }
- 
